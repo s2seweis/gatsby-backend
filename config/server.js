63ca4bd77ab47f5f707a6c8c25cd23f80a1b6285
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-03-08 23:38:30 +0100

    upload

--- config/server.js
+++ config/server.js
@@ -3,7 +3,8 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '6385dd9e9eba3d08fdb6d14bf091e755'),
+      secret: env('ADMIN_JWT_SECRET', 'bJ1LAARMUGLCSaA3InWCmDHmknM0pdkOnVZOR6u3Wkg='),
+      // secret: env('ADMIN_JWT_SECRET', '6385dd9e9eba3d08fdb6d14bf091e755'),
     },
   },
 });
