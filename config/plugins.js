2de09df0f9d2396a758e659daf80e59a1f4f31b6
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-04-05 13:55:11 +0200

    upload

--- config/plugins.js
+++ config/plugins.js
@@ -12,24 +12,13 @@ module.exports = ({ env })=>({
 
 
 
-// module.exports = ({ env }) => ({
-//     upload: {
-//       provider: 'aws-s3',
-//       providerOptions: {
-//         accessKeyId: "AKIA2NVUY27TM2X7S5LT",
-//         secretAccessKey: "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",
-//         region: 'eu-central-1',
-//         params: {
-//           Bucket: 'strapiuploaddemo996',
-//         },
-//       },
-//     },
-//   });
+
 
 
   module.exports = ({ env }) => ({
     upload: {
       provider: 'aws-s3',
+
       providerOptions: {
         accessKeyId: env('AWS_ACCESS_KEY_ID'),
         secretAccessKey: env('AWS_ACCESS_SECRET'),
