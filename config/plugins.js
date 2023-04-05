33b383074075adf5fdb9bd304b701f235df1cae7
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-04-05 13:51:26 +0200

    change config vars

--- config/plugins.js
+++ config/plugins.js
@@ -12,34 +12,34 @@ module.exports = ({ env })=>({
 
 
 
-module.exports = ({ env }) => ({
+// module.exports = ({ env }) => ({
+//     upload: {
+//       provider: 'aws-s3',
+//       providerOptions: {
+//         accessKeyId: "AKIA2NVUY27TM2X7S5LT",
+//         secretAccessKey: "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",
+//         region: 'eu-central-1',
+//         params: {
+//           Bucket: 'strapiuploaddemo996',
+//         },
+//       },
+//     },
+//   });
+
+
+  module.exports = ({ env }) => ({
     upload: {
       provider: 'aws-s3',
       providerOptions: {
-        accessKeyId: "AKIA2NVUY27TM2X7S5LT",
-        secretAccessKey: "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",
-        region: 'eu-central-1',
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: 'aws-region',
         params: {
           Bucket: 'strapiuploaddemo996',
         },
       },
     },
   });
-
-
-  // module.exports = ({ env }) => ({
-  //   upload: {
-  //     provider: 'aws-s3',
-  //     providerOptions: {
-  //       accessKeyId: env('AWS_ACCESS_KEY_ID', 'AKIA2NVUY27TM2X7S5LT'),
-  //       secretAccessKey: env('AWS_ACCESS_SECRET', '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'),
-  //       region: 'aws-region',
-  //       params: {
-  //         Bucket: 'strapiuploaddemo998',
-  //       },
-  //     },
-  //   },
-  // });
 
 
   
